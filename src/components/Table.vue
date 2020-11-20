e6d49210f6cc8a74b7e52d8a7abd2d2b85a7614d
Evgenii <voznukzhenia@gmail.com>
2020-11-20 19:00:16 +0200

    part of table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,11 +1,33 @@
 <template>
-	<DataTable :value="users">
+	<DataTable
+		:value="users"
+		:paginator="true"
+		:rows="10"
+		dataKey="id"
+		:rowsPerPageOptions="[10, 20, 50, 100]"
+	>
+		<Column
+			v-model="selectedUser"
+			selectionMode="multiple"
+			headerStyle="width: 3em"
+			@click="console.log(selectedUser)"
+		>
+		</Column>
 		<Column
 			v-for="col of columns"
 			:field="col.field"
 			:header="col.header"
 			:key="col.field"
 		>
+		</Column>
+		<Column
+			headerStyle="width: 8rem; text-align: center"
+			bodyStyle="text-align: center; overflow: visible"
+		>
+			<template #body>
+				<Button type="button" icon="pi pi-cog" class="p-button-secondary">
+				</Button>
+			</template>
 		</Column>
 	</DataTable>
 </template>
@@ -24,6 +46,7 @@ export default Vue.extend({
 	},
 	data() {
 		return {
+			selectedUser: null,
 			columns: [
 				{ field: 'name', header: 'Name' },
 				{ field: 'userName', header: 'UserName' },
@@ -31,6 +54,22 @@ export default Vue.extend({
 				{ field: 'role', header: 'Role(s)' }
 			]
 		};
+	},
+	methods: {
+		onRowSelect(event) {
+			this.$toast.add({
+				severity: 'info',
+				detail: `Name: ' + ${event.data.name}`,
+				life: 3000
+			});
+		},
+		onRowUnselect(event) {
+			this.$toast.add({
+				severity: 'warn',
+				detail: `Name: ' + ${event.data.name}`,
+				life: 3000
+			});
+		}
 	}
 });
 </script>
