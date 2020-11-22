207f5750cbef42a067edc586181545bb691b1e30
Evgenii <voznukzhenia@gmail.com>
2020-11-22 13:55:39 +0200

    users in main

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,12 +2,7 @@
 	<div id="app">
 		<AppHeader />
 		<div class="app__content content">
-			<div class="router-link" v-on:click="showUsers" v-if="!isShowUsers">
-				<router-link to="/users">
-					Перейти к Table
-				</router-link>
-			</div>
-			<router-view v-on:show-users="showUsers" />
+			<Users />
 		</div>
 	</div>
 </template>
@@ -29,11 +24,13 @@
 <script lang="">
 import Vue from 'vue';
 import AppHeader from '../components/AppHeader.vue';
+import Users from './Users.vue';
 
 export default Vue.extend({
 	name: 'Main',
 	components: {
-		AppHeader
+		AppHeader,
+		Users
 	},
 	data() {
 		return {
